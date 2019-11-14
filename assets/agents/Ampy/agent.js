eb895fd6ee0151fe31217043ecd1f3e65fcec2b6
Karl Lankester-Carthy <klankester@amplience.com>
2019-11-14 14:44:29 +0000

    ampy

---
+++ assets/agents/Ampy/agent.js
@@ -0,0 +1,195 @@
+clippy.ready('Ampy', {
+    "overlayCount": 1,
+    "sounds": ["1"],
+    "framesize": [124, 93],
+    "animations": {
+        "Greeting": {
+            "frames": [{
+                "duration": 10,
+                "branching": {
+                    "branches": [{
+                        "frameIndex": 0,
+                        "weight": 10
+                    }]
+                },
+            },
+            {
+                "duration": 50,
+                "images": [
+                    [0, 0]
+                ],
+            }, 
+            {
+                "duration": 50,
+                "images": [
+                    [124, 0]
+                ],
+            },
+            {
+                "duration": 50,
+                "images": [
+                    [248, 0]
+                ],
+            },
+            {
+                "duration": 50,
+                "images": [
+                    [372, 0]
+                ],
+            },
+            {
+                "duration": 50,
+                "images": [
+                    [496, 0]
+                ],
+            },
+            {
+                "duration": 50,
+                "images": [
+                    [620, 0]
+                ],
+            },
+            {
+                "duration": 50,
+                "images": [
+                    [744, 0]
+                ],
+            }, 
+            {
+                "duration": 50,
+                "images": [
+                    [868, 0]
+                ],
+            },
+            {
+                "duration": 50,
+                "images": [
+                    [992, 0]
+                ],
+            },
+            {
+                "duration": 80,
+                "images": [
+                    [0, 0]
+                ],
+            }]
+        },
+        "Congratulate": {
+            "frames": [{
+                "duration": 150,
+                "images": [
+                    [1240, 0]
+                ],
+            }, {
+                "duration": 150,
+                "images": [
+                    [1364, 0]
+                ],
+            }, {
+                "sound": "1",
+                "duration": 150,
+                "images": [
+                    [1488, 0]
+                ],
+            }, {
+                "duration": 150,
+                "images": [
+                    [1612, 0]
+                ],
+            }, {
+                "duration": 150,
+                "images": [
+                    [1736, 0]
+                ],
+            }, {
+                "duration": 150,
+                "images": [
+                    [1860, 0]
+                ],
+            }, {
+                "duration": 150,
+                "images": [
+                    [1984, 0]
+                ],
+            }, {
+                "duration": 150,
+                "images": [
+                    [2108, 0]
+                ],
+            }, {
+                "duration": 150,
+                "images": [
+                    [2232, 0]
+                ],
+            }, 
+            {
+                "duration": 150,
+                "images": [
+                    [2356, 0]
+                ],
+            }, 
+            {
+                "duration": 150,
+                "images": [
+                    [2480, 0]
+                ],
+            }, 
+            {
+                "duration": 250,
+                "images": [
+                    [2604, 0]
+                ],
+            },
+            {
+                "duration": 150,
+                "images": [
+                    [0, 0]
+                ],
+            },
+        ]
+        },
+        "Hide": {
+            "frames": [{
+                "duration": 150,
+                "images": [
+                    [3100, 0]
+                ],
+            }]
+        },
+        "Pleased": {
+            "frames": [{
+                "duration": 250,
+                "images": [
+                    [2852, 0]
+                ],
+            },
+            {
+                "duration": 250,
+                "images": [
+                    [2976, 0]
+                ],
+            },]
+        },
+        "Thinking": {
+            "frames": [{
+                "duration": 250,
+                "images": [
+                    [3224, 0]
+                ],
+            },
+            {
+                "duration": 250,
+                "images": [
+                    [3348, 0]
+                ],
+            },
+            {
+                "duration": 250,
+                "images": [
+                    [3472, 0]
+                ],
+            }]
+        }
+
+    }
+});
